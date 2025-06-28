82a9a70055d9bcc24f87503424525db198d6915f
Naman Mishra <namancse12411094@iiitsonepat.ac.in>
2025-06-28 16:20:10 +0530

    Add files via upload

---
+++ Studentpracticeexercise/StudentExercise1.cpp
@@ -0,0 +1,15 @@
+//Program to Calculate Area of Circle
+#include<iostream>
+
+int main(){
+ float radius;
+ float area;
+
+ std::cout<<"Enrer Radius of a Circle";
+ std::cin>>radius;
+
+ area=3.1425f*radius*radius;
+
+ std::cout<<"Area of a Circle is "<<area<<std::endl;
+
+}
