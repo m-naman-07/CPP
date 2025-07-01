7d3313b70cad5586da249ee046dc820260ecd06a
Naman Mishra <namancse12411094@iiitsonepat.ac.in>
2025-07-02 02:55:03 +0530

    Add files via upload

---
+++ Studentpracticeexercise/StudentExercise5.cpp
@@ -0,0 +1,27 @@
+//print reverse no. and check if it is a palindrome
+#include <iostream>
+
+int main(){
+
+    int r{};
+    int n{};
+    int rev{};
+
+    std::cout<<"Enter your no."<<std::flush;
+    std::cin>>n;
+
+    int temp{n};
+    while(temp>0){
+        r=temp%10;
+        temp=temp/10;
+        rev=rev*10 + r;
+        
+    }
+    std::cout<<"Reverse of "<< n<<" is "<<rev<<".\n";
+    if(rev==n){
+        std::cout<<"Entered number is a palindrome."<<std::endl;
+    }
+    else{
+        std::cout<<"Entered number is not a palindrome."<<std::endl;
+    }
+}
