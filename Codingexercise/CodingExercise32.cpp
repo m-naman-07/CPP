3875d9ec8595c79d658e057fc9fbf056401fab84
Naman Mishra <namancse12411094@iiitsonepat.ac.in>
2025-07-05 21:54:00 +0530

    Add files via upload

---
+++ Codingexercise/CodingExercise32.cpp
@@ -0,0 +1,17 @@
+/*
+Display all Elements using For Each loop
+Display all elements of Array using foreach loop, Display number with spaces
+*/
+
+#include<iostream>
+using namespace std;
+
+void Display()
+{
+    int A[]={2,4,6,8,10};
+    
+    for(int x:A){
+        cout<<x<<" ";
+    }
+    
+}
