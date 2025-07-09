b7f678c4f5e922445ae6a17d017920269f86329b
Naman Mishra <namancse12411094@iiitsonepat.ac.in>
2025-07-09 11:01:33 +0530

    Add files via upload

--- Section wise/section9/2.HeapMemoryAllocation.cpp
+++ Section wise/section9/2.HeapMemoryAllocation.cpp
@@ -266,19 +266,3 @@ Program exits	                OS reclaims memory
 (But you can't reuse it)
 
 */
-
-//program for dynamic allocation
-
-# include<iostream>
-using namespace std;
-    
-
-int main(){
-
-	int size;
-	cout<<"enter no of element";
-	cin>>size;
-	int a[size];
-	cout<<sizeof a<<endl;
-    	return 0;
-}
