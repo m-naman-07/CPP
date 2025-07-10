b13ebd0617b29b39109d79a0f98a7a146eb49e86
Naman Mishra <namancse12411094@iiitsonepat.ac.in>
2025-07-10 20:15:24 +0530

    Add files via upload

--- Section wise/section5/1.addtwonumbers.cpp
+++ Section wise/section5/1.addtwonumbers.cpp
@@ -1,18 +1,16 @@
 // WAP to take input and add two numbers
 #include <iostream>
-
+using namespace std;
 int main() {
     
     int a{};
     int b{};
     int c{};
     
-    std::cout << "Enter two nos a & b" << std::endl;
-    std::cin >> a >> b;
+    cout << "Enter two nos a & b:" << flush;
+    cin >> a >> b;
 
     c = a+b;
 
-    std::cout<<"Sum of a and b is equal to "<<c<<"\n";
-
-    return 0;
+    cout<<"Sum of a and b is equal to "<<c<<"\n";
 }
