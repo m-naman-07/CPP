b6386a92146fa49f987d57a2e2a1ff1fb28b3db9
Naman Mishra <namancse12411094@iiitsonepat.ac.in>
2025-07-10 20:15:44 +0530

    Add files via upload

---
+++ 2.printenteredname.cpp
@@ -0,0 +1,24 @@
+// WAP to get the name from user and print it
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    string name;
+    cout << "May I know your name?" <<flush;
+    /*
+    // std::cin >> name;
+    can't use this coz 
+    cin >> reads only until the first whitespace
+
+    That includes:
+	•	Space ' '
+	•	Tab \t
+	•	Newline \n
+    Leaves the rest (like "world") in the input buffer
+    Use getline() to read the whole line
+    */
+    getline(cin ,name);
+    cout<< "Welcome,"<<name<<endl;
+
+}
